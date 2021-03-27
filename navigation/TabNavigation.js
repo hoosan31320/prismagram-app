b3e92624ea71b0afe9fb87018080f61d4b8c3dcf
hoosan31320 <hoos31320@gmail.com>
2021-03-27 15:04:10 +0700

    Photo Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Search from "../screens/Search";
@@ -10,27 +9,26 @@ import Profile from "../screens/Profile";
 const Tab = createBottomTabNavigator();
 
 export default () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home}
-        options={{ tabBarIcon: ({ focused, size, color }) => {} }}
-      />
-      <Tab.Screen name="Search" component={Search}
-        options={{ tabBarIcon: ({ focused, size, color }) => {} }}
-      />
-      <Tab.Screen name="View" component={View}
-        listeners={({ navigation, route }) => ({
-          tabPress: (e) => {
-            console.log("Add");
-          },
-        })}
-      />
-      <Tab.Screen name="Notifications" component={Notifications}
-        options={{ tabBarIcon: ({ focused, size, color }) => {} }}
-      />
-      <Tab.Screen name="Profile" component={Profile}
-        options={{ tabBarIcon: ({ focused, size, color }) => {} }}
-      />
-    </Tab.Navigator>
-  </NavigationContainer>
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={Home}
+      options={{ tabBarIcon: ({ focused, size, color }) => {} }}
+    />
+    <Tab.Screen name="Search" component={Search}
+      options={{ tabBarIcon: ({ focused, size, color }) => {} }}
+    />
+    <Tab.Screen name="View" component={View}
+      listeners={({ navigation, route }) => ({
+        tabPress: (e) => {
+          e.preventDefault();
+          navigation.navigate("PhotoNavigation");
+        },
+      })}
+    />
+    <Tab.Screen name="Notifications" component={Notifications}
+      options={{ tabBarIcon: ({ focused, size, color }) => {} }}
+    />
+    <Tab.Screen name="Profile" component={Profile}
+      options={{ tabBarIcon: ({ focused, size, color }) => {} }}
+    />
+  </Tab.Navigator>
 );
