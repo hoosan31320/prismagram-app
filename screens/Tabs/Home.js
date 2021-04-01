db75db53572e899ee4c9022ace8327dbef639afb
hoosan31320 <hoos31320@gmail.com>
2021-04-01 08:25:08 +0700

    Post Component Header

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
 import { useQuery } from "react-apollo-hooks";
+import Post from "../../components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -35,17 +36,10 @@ const FEED_QUERY = gql`
   }
 `;
 
-const View = styled.View`
-  justify-content: center;
-  align-items: center;
-  flex: 1;
-`;
-
-const Text = styled.Text``;
-
 export default () => {
   const [refreshing, setRefreshing] = useState(false);
   const { loading, data, refetch } = useQuery(FEED_QUERY);
+  console.log(data);
   const refresh = async () => {
     try{
       setRefreshing(true);
@@ -62,7 +56,13 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      { loading ? <Loader /> : <Text>Hello</Text> }
+      { loading ? (
+        <Loader />
+      ) : (
+      data &&
+      data.seeFeed &&
+      data.seeFeed.map(post => <Post key={post.id} {...post} />)  
+      )}
     </ScrollView>
   );
 };
